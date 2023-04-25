941d91366d6350a6705582a9893251d793e85c0a
Angelo Verlain <geoangercola@gmail.com>
2023-04-25 15:52:09 +0200

    search allow pagination

--- src/components/search.ts
+++ src/components/search.ts
@@ -4,6 +4,7 @@ import GLib from "gi://GLib";
 import Adw from "gi://Adw";
 
 import {
+  get_more_search_results,
   search,
   SearchAlbum,
   SearchArtist,
@@ -15,6 +16,7 @@ import {
   SearchVideo,
 } from "../muse.js";
 import { load_thumbnails } from "./webimage.js";
+import { Paginator } from "./paginator.js";
 
 export class InlineCard extends Gtk.ListBoxRow {
   static {
@@ -233,36 +235,82 @@ export class SearchSection extends Gtk.Box {
 
 export class SearchPage extends Gtk.Box {
   static {
-    GObject.registerClass(this);
+    GObject.registerClass({
+      GTypeName: "SearchPage",
+      Template:
+        "resource:///org/example/TypescriptTemplate/components/search/page.ui",
+      InternalChildren: ["scrolled", "content", "sections"],
+    }, this);
   }
 
-  scrolled: Gtk.ScrolledWindow;
-  content: Gtk.Box;
+  _scrolled!: Gtk.ScrolledWindow;
+  _content!: Gtk.Box;
+  _sections!: Gtk.Box;
+
+  paginator: Paginator;
+
+  results?: SearchResults;
+  args: Parameters<typeof search> = [""];
 
   constructor() {
     super({
       orientation: Gtk.Orientation.VERTICAL,
     });
 
-    this.scrolled = new Gtk.ScrolledWindow({
-      vexpand: true,
+    this.paginator = new Paginator();
+    this.paginator.connect("activate", () => {
+      this.search_more();
     });
-    this.content = Gtk.Box.new(Gtk.Orientation.VERTICAL, 0);
+    this._content.append(this.paginator);
+  }
 
-    this.scrolled.set_child(this.content);
-    this.append(this.scrolled);
+  show_results(results: SearchResults, args: Parameters<typeof search>) {
+    this.results = results;
+    this.args = args;
+
+    this.paginator.set_reveal_child(results.continuation != null);
+
+    results.categories.forEach((category) => {
+      const section = new SearchSection({
+        args,
+        show_more: results.categories.length > 1,
+      });
+      section.set_category(category);
+      this._sections.append(section);
+    });
   }
 
   search(...args: Parameters<typeof search>) {
     return search(...args).then((results) => {
-      results.categories.forEach((category) => {
-        const section = new SearchSection({
-          args,
-          show_more: results.categories.length > 1,
-        });
-        section.set_category(category);
-        this.content.append(section);
-      });
+      this.results = results;
+      this.show_results(results, args);
     });
+  }
+
+  loading = false;
+
+  search_more() {
+    if (this.loading || !this.results || !this.results?.continuation) return;
+
+    this.loading = true;
+
+    get_more_search_results(this.results.continuation, this.args[1] ?? {})
+      .then((results) => {
+        this.results!.continuation = results.continuation;
+
+        const first_section = this._sections.get_first_child() as SearchSection;
+
+        if (!first_section || !(first_section instanceof SearchSection)) {
+          return;
+        } else {
+          results.results.forEach((content) => {
+            this.results!.categories[0].results.push(content);
+            first_section.add_content(content);
+          });
+        }
+
+        this.paginator.loading = this.loading = false;
+        this.paginator.set_reveal_child(results.continuation != null);
+      });
   }
 }
