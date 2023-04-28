c25015eddf6798e60f79e0edab1c4d7865f38be3
Angelo Verlain <geoangercola@gmail.com>
2023-04-28 12:02:01 +0200

    added top result in search

--- src/pages/search.ts
+++ src/pages/search.ts
@@ -14,6 +14,10 @@ import {
   SearchResults,
   SearchSong,
   SearchVideo,
+  TopResult,
+  TopResultAlbum,
+  TopResultArtist,
+  TopResultSong,
 } from "../muse.js";
 import { load_thumbnails } from "../components/webimage.js";
 import { Paginator } from "../components/paginator.js";
@@ -154,6 +158,122 @@ export class InlineCard extends Gtk.ListBoxRow {
   }
 }
 
+export class TopResultCard extends Gtk.FlowBoxChild {
+  static {
+    GObject.registerClass({
+      GTypeName: "TopResult",
+      Template:
+        "resource:///org/example/TypescriptTemplate/components/search/topresult.ui",
+      InternalChildren: [
+        "avatar",
+        "image",
+        "image_overlay",
+        "title",
+        "explicit",
+        "label_box",
+        "type",
+        "type_box",
+        // "second-line",
+      ],
+    }, this);
+  }
+
+  constructor() {
+    super();
+  }
+
+  _avatar!: Adw.Avatar;
+  _image!: Gtk.Image;
+  _image_overlay!: Gtk.Overlay;
+  _title!: Gtk.Label;
+  _explicit!: Gtk.Label;
+  _label_box!: Gtk.Box;
+  _type!: Gtk.Label;
+  _type_box!: Gtk.Box;
+  _second_line!: Gtk.Box;
+
+  image_size = 100;
+  add_subsequent_middots = false;
+
+  result?: TopResult;
+
+  show_type(show: boolean) {
+    this._type_box.visible = show;
+  }
+
+  show_avatar(show: boolean) {
+    this._avatar.visible = show;
+    this._image_overlay.visible = !show;
+  }
+
+  insert_middot(force = false) {
+    if (this.add_subsequent_middots || force) {
+      this._label_box.append(Gtk.Label.new("·"));
+    } else {
+      this.add_subsequent_middots = true;
+    }
+  }
+
+  insert_only_text(text: string) {
+    this._label_box.append(Gtk.Label.new(text));
+  }
+
+  insert_text(text: string) {
+    this.insert_middot();
+    this.insert_only_text(text);
+  }
+
+  private set_song_or_video(track: TopResultSong) {
+    this.result = track;
+
+    load_thumbnails(this._image, track.thumbnails, this.image_size);
+
+    this._title.label = track.title;
+    this._explicit.set_visible(track.isExplicit);
+
+    track.artists.forEach((artist) => {
+      this.insert_text(artist.name);
+    });
+
+    if (track.duration) this.insert_text(track.duration);
+  }
+
+  set_song(song: TopResultSong) {
+    this.set_song_or_video(song);
+    this._type.label = "Song";
+  }
+
+  set_album(album: TopResultAlbum) {
+    this.result = album;
+
+    load_thumbnails(this._image, album.thumbnails, this.image_size);
+
+    this._title.label = album.title;
+    this._explicit.set_visible(album.isExplicit);
+
+    this._type.label = album.album_type;
+
+    album.artists.forEach((artist) => {
+      this.insert_text(artist.name);
+    });
+  }
+
+  set_artist(artist: TopResultArtist) {
+    this.result = artist;
+
+    this.show_avatar(true);
+    load_thumbnails(this._avatar, artist.thumbnails, this.image_size);
+
+    this._title.label = artist.name;
+
+    this.show_type(false);
+
+    if (artist.subscribers) {
+      this.insert_only_text(`${artist.subscribers} subscribers`);
+    }
+  }
+}
+
 function search_args_to_url(query: string, options: SearchOptions = {}) {
   const params = new URLSearchParams(Object.entries(options))
     .toString();
@@ -252,12 +372,83 @@ export class SearchSection extends Gtk.Box {
   }
 }
 
+export class TopResultSection extends Gtk.Box {
+  static {
+    GObject.registerClass({
+      GTypeName: "TopResultSection",
+      Template:
+        "resource:///org/example/TypescriptTemplate/components/search/topresultsection.ui",
+      InternalChildren: ["title", "flowbox", "content"],
+    }, this);
+  }
+
+  _title!: Gtk.Label;
+  _flowbox!: Gtk.FlowBox;
+  _content!: Gtk.ListBox;
+
+  constructor() {
+    super();
+  }
+
+  add_more_content(content: SearchContent) {
+    let card = new InlineCard();
+
+    switch (content.type) {
+      case "song":
+        card.set_song(content);
+        break;
+      case "video":
+        card.set_video(content);
+        break;
+      case "album":
+        card.set_album(content);
+        break;
+      case "artist":
+        card.set_artist(content);
+        break;
+      case "playlist":
+        card.set_playlist(content);
+        break;
+      default:
+        console.error("Unknown search content type", content.type);
+        return;
+    }
+
+    this._content.append(card);
+  }
+
+  show_top_result(top_result: TopResult) {
+    let card = new TopResultCard();
+
+    switch (top_result.type) {
+      case "song":
+        card.set_song(top_result);
+        break;
+      case "album":
+        card.set_album(top_result);
+        break;
+      case "artist":
+        card.set_artist(top_result);
+        break;
+        // default:
+        //   console.error("Unknown top result type", top_result.type);
+        //   return;
+    }
+
+    if (top_result.more) {
+      top_result.more.forEach(this.add_more_content.bind(this));
+    }
+
+    this._flowbox.prepend(card);
+    // card.insert_before(this._flowbox, this._content);
+  }
+}
+
 export class SearchPage extends Gtk.Box {
   static {
     GObject.registerClass({
       GTypeName: "SearchPage",
-      Template:
-        "resource:///org/example/TypescriptTemplate/pages/search.ui",
+      Template: "resource:///org/example/TypescriptTemplate/pages/search.ui",
       InternalChildren: ["scrolled", "content", "sections"],
     }, this);
   }
@@ -288,6 +479,13 @@ export class SearchPage extends Gtk.Box {
     this.args = args;
 
     this.paginator.set_reveal_child(results.continuation != null);
+
+    if (results.top_result) {
+      const top_result = new TopResultSection();
+
+      top_result.show_top_result(results.top_result);
+      this._sections.append(top_result);
+    }
 
     results.categories.forEach((category) => {
       const section = new SearchSection({
