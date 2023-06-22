e5febc720132d3f93f8e26d3cf1f0b5371cd959c
Angelo Verlain <geoangercola@gmail.com>
2023-06-22 14:55:03 +0200

    fix translations
    fix #15

--- types/ambient.d.ts
+++ types/ambient.d.ts
@@ -1,4 +1,5 @@
 declare function _(id: string): string;
+declare function C_(context: string, id: string): string;
 declare function print(args: string): void;
 declare function log(obj: object, others?: object[]): void;
 declare function log(msg: string, substitutions?: any[]): void;
@@ -31,4 +32,22 @@ declare class TextEncoder {
 declare class TextDecoder {
   constructor(encoding?: string);
   decode(input?: Uint8Array): string;
+}
+
+declare module imports {
+  const format: {
+    format(this: String, ...args: any[]): string;
+    printf(fmt: string, ...args: any[]): string;
+    vprintf(fmt: string, args: any[]): string;
+  };
+}
+
+
+declare module "gettext" {
+  export function gettext(id: string): string;
+  export function ngettext(
+    singular: string,
+    plural: string,
+    n: number,
+  ): string;
 }
