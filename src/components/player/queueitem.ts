cf98845c00eb98835ae24c13cd2077a24c282108
Angelo Verlain <geoangercola@gmail.com>
2023-06-22 20:03:34 +0200

    use link subtitle on queueitem
    to prevent the queue sidebar from overexpanding

--- src/components/player/queueitem.ts
+++ src/components/player/queueitem.ts
@@ -6,6 +6,7 @@ import GLib from "gi://GLib";
 import { ArtistRun } from "../../muse.js";
 import { load_thumbnails } from "../webimage.js";
 import { QueueTrack } from "libmuse/types/parsers/queue.js";
+import { pretty_subtitles } from "src/util/text.js";
 
 export class QueueItem extends Gtk.Box {
   static {
@@ -18,87 +19,32 @@ export class QueueItem extends Gtk.Box {
         "image",
         "title",
         "explicit",
-        "explicit_flowbox",
-        "second_line",
-        "image",
+        "subtitle",
       ],
     }, this);
   }
 
   item?: QueueTrack;
 
-  _play_button!: Gtk.Button;
-  _image!: Gtk.Image;
-  _title!: Gtk.Label;
-  _explicit!: Gtk.Image;
-  _explicit_flowbox!: Gtk.FlowBox;
-  _second_line!: Gtk.Box;
+  private _play_button!: Gtk.Button;
+  private _image!: Gtk.Image;
+  private _title!: Gtk.Label;
+  private _explicit!: Gtk.Image;
+  private _subtitle!: Gtk.Label;
 
   constructor() {
     super({});
-  }
 
-  add_subsequent_middots = false;
+    this._subtitle.connect("activate-link", (_, uri) => {
+      if (uri && uri.startsWith("muzika:")) {
+        this.activate_action(
+          "navigator.visit",
+          GLib.Variant.new_string(uri),
+        );
 
-  insert_middot(force = false) {
-    const label = Gtk.Label.new("·");
-
-    label.add_css_class("dim-label");
-
-    const flowchild = new Gtk.FlowBoxChild({
-      halign: Gtk.Align.START,
-      child: label,
+        return true;
+      }
     });
-
-    if (this.add_subsequent_middots || force) {
-      this._second_line.append(flowchild);
-    } else {
-      this.add_subsequent_middots = true;
-    }
-  }
-
-  add_artist_only(artist: ArtistRun) {
-    let child: Gtk.Widget;
-
-    if (artist.id) {
-      const button = new Gtk.Button({
-        label: artist.name,
-        cursor: Gdk.Cursor.new_from_name("pointer", null),
-      });
-
-      button.add_css_class("inline");
-      button.add_css_class("flat");
-      button.add_css_class("link");
-      button.add_css_class("dim-label");
-
-      button.action_name = "navigator.visit";
-      button.action_target = GLib.Variant.new(
-        "s",
-        `muzika:artist:${artist.id}`,
-      );
-
-      child = button;
-    } else {
-      const label = Gtk.Label.new(artist.name);
-
-      label.add_css_class("dim-label");
-
-      child = label;
-    }
-
-    const flowchild = new Gtk.FlowBoxChild({
-      halign: Gtk.Align.START,
-      child: child,
-    });
-
-    flowchild.add_css_class("no-padding");
-
-    this._second_line.append(flowchild);
-  }
-
-  add_author(artist: ArtistRun) {
-    this.insert_middot();
-    this.add_artist_only(artist);
   }
 
   set_track(item: QueueTrack) {
@@ -106,13 +52,12 @@ export class QueueItem extends Gtk.Box {
 
     this._title.set_label(item.title);
 
-    if (item.artists && item.artists.length > 0) {
-      item.artists.map((artist) => {
-        this.add_author(artist);
-      });
-    }
+    const subtitles = pretty_subtitles(item.artists);
 
-    this._explicit_flowbox.set_visible(item.isExplicit);
+    this._subtitle.set_markup(subtitles.markup);
+    this._subtitle.tooltip_text = subtitles.plain;
+
+    this._explicit.set_visible(item.isExplicit);
 
     load_thumbnails(this._image, item.thumbnails, 48);
   }
