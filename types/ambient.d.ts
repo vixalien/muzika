de12e9b99c8b438b5973572cf63890f0d946495d
Angelo Verlain <geoangercola@gmail.com>
2023-06-22 21:11:48 +0200

    mark some strings as translatable
    Co-authored-by: Sabri Ünal <libreajans@gmail.com>

--- types/ambient.d.ts
+++ types/ambient.d.ts
@@ -8,30 +8,12 @@ declare const pkg: {
   version: string;
   name: string;
 };
-
-declare module console {
-  export function error(...args: any[]): void;
-  export function log(...args: any[]): void;
-  export function warn(...args: any[]): void;
-  export function debug(...args: any[]): void;
-}
-
 declare interface String {
   format(...replacements: string[]): string;
   format(...replacements: number[]): string;
 }
 declare interface Number {
   toFixed(digits: number): number;
-}
-
-declare class TextEncoder {
-  constructor(encoding?: string);
-  encode(input?: string): Uint8Array;
-}
-
-declare class TextDecoder {
-  constructor(encoding?: string);
-  decode(input?: Uint8Array): string;
 }
 
 declare module imports {
