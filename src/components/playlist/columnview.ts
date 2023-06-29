15809813a2844ae215fe1344273ace134315a15a
Angelo Verlain <geoangercola@gmail.com>
2023-06-29 23:23:44 +0200

    show explicit tracks in playlist column view

--- src/components/playlist/columnview.ts
+++ src/components/playlist/columnview.ts
@@ -151,6 +151,38 @@ class ChartRankColumn extends Gtk.ColumnViewColumn {
   }
 }
 
+class TitleBox extends Gtk.Box {
+  static {
+    GObject.registerClass({ GTypeName: "TitleBox" }, this);
+  }
+
+  label: Gtk.Label;
+  explicit: Gtk.Image;
+
+  constructor() {
+    super({
+      spacing: 6,
+    });
+
+    this.label = new Gtk.Label({
+      hexpand: true,
+      ellipsize: Pango.EllipsizeMode.END,
+      lines: 2,
+      xalign: 0,
+    });
+
+    this.explicit = new Gtk.Image({
+      valign: Gtk.Align.CENTER,
+      icon_name: "network-cellular-edge-symbolic",
+    });
+
+    this.explicit.add_css_class("dim-label");
+
+    this.append(this.label);
+    this.append(this.explicit);
+  }
+}
+
 class TitleColumn extends Gtk.ColumnViewColumn {
   static {
     GObject.registerClass({ GTypeName: "TitleColumn" }, this);
@@ -170,23 +202,19 @@ class TitleColumn extends Gtk.ColumnViewColumn {
   }
 
   setup_cb(_factory: Gtk.SignalListItemFactory, list_item: Gtk.ListItem) {
-    const label = new Gtk.Label({
-      hexpand: true,
-      ellipsize: Pango.EllipsizeMode.END,
-      lines: 2,
-      xalign: 0,
-    });
+    const title = new TitleBox();
 
-    list_item.set_child(label);
+    list_item.set_child(title);
   }
 
   bind_cb(_factory: Gtk.SignalListItemFactory, list_item: Gtk.ListItem) {
-    const label = list_item.child as Gtk.Label;
+    const title = list_item.child as TitleBox;
     const container = list_item.item as ObjectContainer<PlaylistItem>;
 
     const playlist_item = container.item!;
 
-    label.label = playlist_item.title;
+    title.label.tooltip_text = title.label.label = playlist_item.title;
+    title.explicit.visible = playlist_item.isExplicit;
   }
 }
 
