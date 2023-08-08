753829a2f863b0662fc4a0e832fb9c22bf0eef5b
Angelo Verlain <geoangercola@gmail.com>
2023-08-08 15:36:01 +0200

    use list view for history

--- src/pages/library/history.ts
+++ src/pages/library/history.ts
@@ -6,6 +6,8 @@ import { get_history, History, PlaylistItem } from "../../muse.js";
 
 import { PlaylistItemCard } from "src/components/playlist/item.js";
 import { EndpointContext, MuzikaComponent } from "src/navigation.js";
+import { InlineSongListView } from "src/components/carousel/view/inlinesonglist.js";
+import { PlayableContainer } from "src/util/playablelist.js";
 
 interface PlaylistItemWithCategory extends PlaylistItem {
   category?: string;
@@ -43,6 +45,8 @@ class HistoryTitle extends Gtk.Box {
   }
 }
 
+InlineSongListView;
+
 export class HistoryPage extends Adw.Bin
   implements MuzikaComponent<History, HistoryState> {
   static {
@@ -50,28 +54,28 @@ export class HistoryPage extends Adw.Bin
       GTypeName: "HistoryPage",
       Template:
         "resource:///com/vixalien/muzika/ui/components/library/history.ui",
-      InternalChildren: ["list"],
+      InternalChildren: ["list_view"],
     }, this);
   }
 
-  private _list!: Gtk.ListBox;
+  private _list_view!: InlineSongListView;
 
   results?: History;
 
   constructor() {
     super();
 
-    this._list.set_header_func((row) => {
-      const card = row as PlaylistItemCard;
-      const item = card.item as PlaylistItemWithCategory;
-      const category = item?.category;
+    // this._list.set_header_func((row) => {
+    //   const card = row as PlaylistItemCard;
+    //   const item = card.item as PlaylistItemWithCategory;
+    //   const category = item?.category;
 
-      if (category) {
-        const title = new HistoryTitle({ title: category });
+    //   if (category) {
+    //     const title = new HistoryTitle({ title: category });
 
-        row.set_header(title);
-      }
-    });
+    //     row.set_header(title);
+    //   }
+    // });
   }
 
   loading = false;
@@ -83,24 +87,30 @@ export class HistoryPage extends Adw.Bin
   }
 
   private show_library(library: History) {
-    library.categories.forEach((category) => {
-      category.items
-        .filter(Boolean)
-        .forEach((item, index) => {
-          if (index === 0) {
-            item = {
-              ...item,
-              category: category.title,
-            } as PlaylistItemWithCategory;
-          }
+    const items_with_category = library.categories.reduce(
+      (acc, category) => {
+        return [
+          ...acc,
+          ...category.items.map((item, index) => {
+            if (index === 0) {
+              return {
+                ...item,
+                category: category.title,
+              } as PlaylistItemWithCategory;
+            } else {
+              return item;
+            }
+          }),
+        ];
+      },
+      [] as PlaylistItemWithCategory[],
+    );
 
-          const card = new PlaylistItemCard();
-
-          card.set_item(item);
-
-          this._list.append(card);
-        });
-    });
+    this._list_view.items.splice(
+      0,
+      0,
+      items_with_category.map(PlayableContainer.new_from_playlist_item),
+    );
   }
 
   get_state() {
