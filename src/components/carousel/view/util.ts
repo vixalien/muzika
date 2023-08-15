77828811fc881722b2f1172a19900c66037b5254
Angelo Verlain <geoangercola@gmail.com>
2023-08-15 11:41:28 +0200

    fix libary artists and channels not being clickable

--- src/components/carousel/view/util.ts
+++ src/components/carousel/view/util.ts
@@ -1,6 +1,8 @@
 import GLib from "gi://GLib";
+import Gtk from "gi://Gtk?version=4.0";
+import Gio from "gi://Gio";
 
-import { SearchContent } from "src/muse";
+import { ParsedMoodOrGenre, SearchContent } from "src/muse";
 
 import { MixedCardItem } from "src/components/library/mixedcard";
 import { FlatGridView } from "./flatgrid";
@@ -16,88 +18,53 @@ export enum FlatViewChildType {
   MIXED_CARD,
 }
 
-export function flat_view_activate_cb<View extends FlatGridView | FlatListView>(
-  view: View,
-  position: number,
+function activate_inline_song(
+  view: Gtk.Widget,
+  song: InlineSong,
 ) {
-  const item = view.items.get_item(position)?.object;
+  if (song.videoId) {
+    view.activate_action(
+      "queue.play-song",
+      GLib.Variant.new_string(
+        song.videoId,
+      ),
+    );
+  }
+}
 
-  if (!item) return;
-
+function activate_search_content(
+  view: Gtk.Widget,
+  search_content: SearchContent,
+) {
   let uri: string | null = null;
 
-  switch (view.child_type) {
-    case FlatViewChildType.MIXED_CARD:
-      const child = item as MixedCardItem;
-      switch (child.type) {
-        case "playlist":
-        case "watch-playlist":
-          uri = `playlist:${child.playlistId}`;
-          break;
-        case "artist":
-          uri = `artist:${child.browseId}`;
-          break;
-        case "album":
-          uri = `album:${child.browseId}`;
-          break;
-        case "inline-video":
-        case "song":
-        case "video":
-        case "flat-song":
-          if (child.videoId) {
-            view.activate_action(
-              "queue.play-song",
-              GLib.Variant.new_string(
-                child.videoId,
-              ),
-            );
-          }
-          break;
-      }
+  switch (search_content.type) {
+    case "playlist":
+      uri = `playlist:${search_content.browseId}`;
       break;
-    case FlatViewChildType.INLINE_SONG:
-      const song = item as InlineSong;
-      if (song.videoId) {
-        view.activate_action(
-          "queue.play-song",
-          GLib.Variant.new_string(
-            song.videoId,
-          ),
-        );
-      }
+    case "artist":
+      uri = `artist:${search_content.browseId}`;
       break;
-    case FlatViewChildType.SEARCH_CONTENT:
-      const search_content = item as SearchContent;
-
-      switch (search_content.type) {
-        case "playlist":
-          uri = `playlist:${search_content.browseId}`;
-          break;
-        case "artist":
-          uri = `artist:${search_content.browseId}`;
-          break;
-        case "profile":
-          uri = `channel:${search_content.browseId}`;
-          break;
-        case "album":
-          uri = `album:${search_content.browseId}`;
-          break;
-        case "radio":
-          view.activate_action(
-            "queue.play-playlist",
-            GLib.Variant.new_string(
-              `${search_content.playlistId}?video=${search_content.videoId}`,
-            ),
-          );
-          break;
-        case "song":
-        case "video":
-          view.activate_action(
-            "queue.play-song",
-            GLib.Variant.new_string(search_content.videoId),
-          );
-          break;
-      }
+    case "profile":
+      uri = `channel:${search_content.browseId}`;
+      break;
+    case "album":
+      uri = `album:${search_content.browseId}`;
+      break;
+    case "radio":
+      view.activate_action(
+        "queue.play-playlist",
+        GLib.Variant.new_string(
+          `${search_content.playlistId}?video=${search_content.videoId}`,
+        ),
+      );
+      break;
+    case "song":
+    case "video":
+      view.activate_action(
+        "queue.play-song",
+        GLib.Variant.new_string(search_content.videoId),
+      );
       break;
   }
 
@@ -109,13 +76,28 @@ export function flat_view_activate_cb<View extends FlatGridView | FlatListView>(
   }
 }
 
-export function mixed_card_activate_cb<
-  View extends CarouselGridView | CarouselListView,
->(view: View, position: number) {
+export function flat_view_activate_cb<View extends FlatGridView | FlatListView>(
+  view: View,
+  position: number,
+) {
   const item = view.items.get_item(position)?.object;
 
   if (!item) return;
 
+  switch (view.child_type) {
+    case FlatViewChildType.MIXED_CARD:
+      return activate_mixed_card(view, item as MixedCardItem);
+    case FlatViewChildType.INLINE_SONG:
+      return activate_inline_song(view, item as InlineSong);
+    case FlatViewChildType.SEARCH_CONTENT:
+      return activate_search_content(view, item as SearchContent);
+  }
+}
+
+function activate_mixed_card(
+  view: Gtk.Widget,
+  item: MixedCardItem,
+) {
   let uri: string | null = null;
 
   switch (item.type) {
@@ -124,10 +106,16 @@ export function mixed_card_activate_cb<
       uri = `playlist:${item.playlistId}`;
       break;
     case "artist":
+      // library channels are music channels
+    case "channel":
       uri = `artist:${item.browseId}`;
       break;
     case "album":
       uri = `album:${item.browseId}`;
+      break;
+    case "library-artist":
+      // remove MPLA prefix
+      uri = `artist:${item.browseId.slice(4)}`;
       break;
     case "inline-video":
     case "song":
@@ -150,6 +138,16 @@ export function mixed_card_activate_cb<
       GLib.Variant.new_string("muzika:" + uri),
     );
   }
+}
+
+export function mixed_card_activate_cb<
+  View extends CarouselGridView | CarouselListView,
+>(view: View, position: number) {
+  const item = view.items.get_item(position)?.object;
+
+  if (!item) return;
+
+  return activate_mixed_card(view, item);
 }
 
 export function mood_activate_cb(view: CarouselMoodView, position: number) {
