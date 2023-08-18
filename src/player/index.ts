c3fa5c8cd0ae87317720f7a2218429dfbc86049f
Angelo Verlain <geoangercola@gmail.com>
2023-08-18 10:57:39 +0200

    fix some equality mishaps

--- src/player/index.ts
+++ src/player/index.ts
@@ -652,10 +652,8 @@ export class MuzikaPlayer extends MuzikaMediaStream {
 
       // try to seek to the same position
       if (
-        counterpart_videoId && now_playing_videoId &&
-        counterpart_videoId === now_playing_videoId &&
-        current.duration_seconds &&
-        this.timestamp != 0
+        counterpart_videoId && counterpart_videoId === now_playing_videoId &&
+        current.duration_seconds && this.timestamp !== 0
       ) {
         this.initial_seek_to = (this.timestamp / this.duration) *
           (current.duration_seconds * Gst.MSECOND);
