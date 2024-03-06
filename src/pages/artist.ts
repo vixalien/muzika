5940b3774c2e180e7c3bdcddd4967ee8313871d0
Angelo Verlain <geoangercola@gmail.com>
2024-03-06 02:15:37 +0200

    feat: implement the subscribe logic

--- src/pages/artist.ts
+++ src/pages/artist.ts
@@ -4,7 +4,7 @@ import GLib from "gi://GLib";
 import Adw from "gi://Adw";
 import Gio from "gi://Gio";
 
-import { get_artist } from "libmuse";
+import { get_artist, subscribe_artists, unsubscribe_artists } from "libmuse";
 import type { Artist, Category, MixedItem } from "libmuse";
 
 import { Carousel } from "../components/carousel/index.js";
@@ -18,6 +18,7 @@ import {
   VScrollState,
 } from "src/util/scrolled.js";
 import { PlaylistHeader } from "src/components/playlist/header.js";
+import { add_toast } from "src/util/window.js";
 
 interface ArtistState extends VScrollState {
   artist: Artist;
@@ -29,21 +30,27 @@ GObject.type_ensure(PlaylistListView.$gtype);
 export class ArtistPage extends Adw.Bin
   implements MuzikaPageWidget<Artist, ArtistState> {
   static {
-    GObject.registerClass({
-      GTypeName: "ArtistPage",
-      Template: "resource:///com/vixalien/muzika/ui/pages/artist.ui",
-      InternalChildren: [
-        "top_songs",
-        "more_top_songs",
-        "playlist_item_view",
-        "header",
-        "carousels",
-        "menu",
-        "scrolled",
-        "shuffle_button",
-        "radio_button",
-      ],
-    }, this);
+    GObject.registerClass(
+      {
+        GTypeName: "ArtistPage",
+        Template: "resource:///com/vixalien/muzika/ui/pages/artist.ui",
+        InternalChildren: [
+          "top_songs",
+          "more_top_songs",
+          "playlist_item_view",
+          "header",
+          "carousels",
+          "menu",
+          "scrolled",
+          "shuffle_button",
+          "radio_button",
+          "subscribe_button",
+          "subscribe_label",
+          "subscribers",
+        ],
+      },
+      this,
+    );
   }
 
   artist?: Artist;
@@ -57,6 +64,9 @@ export class ArtistPage extends Adw.Bin
   private _scrolled!: Gtk.ScrolledWindow;
   private _shuffle_button!: Gtk.Button;
   private _radio_button!: Gtk.Button;
+  private _subscribe_button!: Gtk.Button;
+  private _subscribe_label!: Gtk.Label;
+  private _subscribers!: Gtk.Label;
 
   model = new PlayableList();
 
@@ -68,11 +78,11 @@ export class ArtistPage extends Adw.Bin
 
   show_top_songs(songs: Artist["songs"]) {
     this._playlist_item_view.playlistId = songs.browseId ?? undefined;
-    this._playlist_item_view.show_artists = songs.results.some((track) =>
-      track.artists.length > 0
+    this._playlist_item_view.show_artists = songs.results.some(
+      (track) => track.artists.length > 0,
     );
-    this._playlist_item_view.show_time = songs.results.some((track) =>
-      track.duration != null
+    this._playlist_item_view.show_time = songs.results.some(
+      (track) => track.duration != null,
     );
 
     if (songs.results && songs.results.length > 0) {
@@ -136,7 +146,76 @@ export class ArtistPage extends Adw.Bin
     this.add_carousel(_("Playlists"), artist.playlists);
     this.add_carousel(_("Fans might also like"), artist.related);
 
+    this.update_subscribe_button();
+
     this.setup_menu();
+  }
+
+  private update_subscribe_button() {
+    if (!this.artist) return;
+
+    if (this.artist?.subscribed) {
+      this._subscribe_label.label = _("Subscribed");
+      this._subscribe_button.remove_css_class("subscribe");
+    } else {
+      this._subscribe_button.add_css_class("subscribe");
+      this._subscribe_label.label = _("Subscribe");
+    }
+
+    this._subscribers.visible = !!this.artist.subscribers;
+
+    if (this.artist.subscribers) {
+      this._subscribers.label = this.artist.subscribers;
+    }
+  }
+
+  private subscribe_controller: AbortController | null = null;
+
+  private toggle_subscribe_cb() {
+    if (!this.artist) return;
+
+    if (this.subscribe_controller) {
+      this.subscribe_controller.abort();
+    }
+
+    this.subscribe_controller = new AbortController();
+
+    const old_subscribed = this.artist!.subscribed;
+
+    const options = [
+      [this.artist.channelId],
+      {
+        signal: this.subscribe_controller.signal,
+      },
+    ] as [string[], { signal: AbortSignal }];
+
+    let promise;
+    if (old_subscribed) {
+      promise = unsubscribe_artists(...options);
+    } else {
+      promise = subscribe_artists(...options);
+    }
+
+    // being optimistic...
+    this.artist!.subscribed = !old_subscribed;
+    this.update_subscribe_button();
+
+    promise
+      .then(() => {
+        this.artist!.subscribed = !old_subscribed;
+      })
+      .catch(() => {
+        this.artist!.subscribed = old_subscribed;
+        add_toast(
+          old_subscribed
+            ? _("Couldn't unsubscribe from artist. Try again later")
+            : _("Couldn't subscribe to artist. Try again later"),
+        );
+      })
+      .finally(() => {
+        this.update_subscribe_button();
+        this.subscribe_controller = null;
+      });
   }
 
   private setup_menu() {
@@ -166,7 +245,7 @@ export class ArtistPage extends Adw.Bin
     const carousel = new Carousel();
 
     carousel.setup_more_button(
-      (show_more_button && data.browseId != null && data.params != null)
+      show_more_button && data.browseId != null && data.params != null
         ? `navigator.visit("muzika:artist-albums:${data.browseId}:${data.params}")`
         : null,
     );
