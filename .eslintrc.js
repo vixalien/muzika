c56af53a334f2be1df24b3d315e7f4f0d5845830
Angelo Verlain <geoangercola@gmail.com>
2024-05-09 03:52:56 +0200

    chore: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
+  rules: {
+    "@typescript-eslint/restrict-template-expressions": ["error", {
+      allowNullish: true,
+    }],
+    "prettier/prettier": "error",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  root: true,
+};
