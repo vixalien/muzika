a55d9951f5de7d11927f7fad727806f7b8be8faa
Angelo Verlain <geoangercola@gmail.com>
2024-07-16 02:22:38 +0200

    chore: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  eslintPluginPrettierRecommended,
+);
