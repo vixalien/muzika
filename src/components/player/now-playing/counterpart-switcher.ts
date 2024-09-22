e65a53d535d30e17cfde4cc62e6e23c290b75398
Angelo Verlain <geoangercola@gmail.com>
2024-09-22 04:59:41 +0200

    feat; use AdwToggleGroup for counterpart switcher

--- src/components/player/now-playing/counterpart-switcher.ts
+++ src/components/player/now-playing/counterpart-switcher.ts
@@ -1,15 +1,48 @@
 import GObject from "gi://GObject";
-import Gtk from "gi://Gtk?version=4.0";
+import Adw from "gi://Adw";
 
-export class MuzikaNPCounterpartSwitcher extends Gtk.Box {
+import { get_player } from "src/application";
+
+export class MuzikaNPCounterpartSwitcher extends Adw.Bin {
   static {
     GObject.registerClass(
       {
         GTypeName: "MuzikaNPCounterpartSwitcher",
         Template:
           "resource:///com/vixalien/muzika/ui/components/player/now-playing/counterpart-switcher.ui",
+        InternalChildren: ["toggle_group"],
       },
       this,
     );
+  }
+
+  /// @ts-expect-error outdated types
+  private _toggle_group: Adw.ToggleGroup;
+  private _binding: GObject.Binding | null = null;
+
+  vfunc_map(): void {
+    super.vfunc_map();
+
+    this._binding?.unbind();
+
+    this._binding = get_player().queue.bind_property_full(
+      "current-is-video",
+      this._toggle_group,
+      "active-name",
+      GObject.BindingFlags.SYNC_CREATE | GObject.BindingFlags.BIDIRECTIONAL,
+      (_, current_is_video: boolean) => {
+        return [true, current_is_video ? "video" : "song"];
+      },
+      (_, active_name: "song" | "video") => {
+        return [true, active_name === "video" ? true : false];
+      },
+    );
+  }
+
+  vfunc_unmap(): void {
+    super.vfunc_unmap();
+
+    this._binding?.unbind();
+    this._binding = null;
   }
 }
