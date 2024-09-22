bb85dad038dad3a9b2e8d3c756eafd77c4eb3f65
Angelo Verlain <geoangercola@gmail.com>
2024-09-22 06:04:37 +0200

    fix: counterpart switcher always showing true

--- src/components/player/now-playing/counterpart-switcher.ts
+++ src/components/player/now-playing/counterpart-switcher.ts
@@ -2,6 +2,9 @@ import GObject from "gi://GObject";
 import Adw from "gi://Adw";
 
 import { get_player } from "src/application";
+import { SignalListeners } from "src/util/signal-listener";
+import { ObjectContainer } from "src/util/objectcontainer";
+import { QueueTrack } from "libmuse";
 
 export class MuzikaNPCounterpartSwitcher extends Adw.Bin {
   static {
@@ -18,31 +21,43 @@ export class MuzikaNPCounterpartSwitcher extends Adw.Bin {
 
   /// @ts-expect-error outdated types
   private _toggle_group: Adw.ToggleGroup;
-  private _binding: GObject.Binding | null = null;
+  private listeners = new SignalListeners();
 
   vfunc_map(): void {
     super.vfunc_map();
 
-    this._binding?.unbind();
+    this.listeners.clear();
 
-    this._binding = get_player().queue.bind_property_full(
-      "current-is-video",
-      this._toggle_group,
-      "active-name",
-      GObject.BindingFlags.SYNC_CREATE | GObject.BindingFlags.BIDIRECTIONAL,
-      (_, current_is_video: boolean) => {
-        return [true, current_is_video ? "video" : "song"];
-      },
-      (_, active_name: "song" | "video") => {
-        return [true, active_name === "video" ? true : false];
-      },
+    this.listeners.add_bindings(
+      get_player().queue.bind_property_full(
+        "current-is-video",
+        this._toggle_group,
+        "active-name",
+        GObject.BindingFlags.SYNC_CREATE | GObject.BindingFlags.BIDIRECTIONAL,
+        (_, current_is_video: boolean) => {
+          return [true, current_is_video ? "video" : "song"];
+        },
+        (_, active_name: "song" | "video") => {
+          return [true, active_name === "video" ? true : false];
+        },
+      ),
+      // @ts-expect-error invalid types
+      get_player().queue.bind_property_full(
+        "current",
+        this._toggle_group,
+        "sensitive",
+        GObject.BindingFlags.SYNC_CREATE,
+        (_, current: ObjectContainer<QueueTrack>) => {
+          return [true, current.object.counterpart != null];
+        },
+        null,
+      ),
     );
   }
 
   vfunc_unmap(): void {
     super.vfunc_unmap();
 
-    this._binding?.unbind();
-    this._binding = null;
+    this.listeners.clear();
   }
 }
